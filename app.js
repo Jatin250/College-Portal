d417eb65565e4e0997b0170d7b860eb8e941cac2
Jatin250 <jatinpal25072002@gmail.com>
2024-12-19 12:41:51 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const web = require("./routes/web");
+const connectDb = require("./database/connectDb");
+const fileUpload = require("express-fileupload");
+
+// image upload
+app.use(fileUpload({ useTempFiles: true }));
+
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
+
+// connect flash and session
+const session = require("express-session");
+const flash = require("connect-flash");
+
+// messages
+app.use(
+  session({
+    secret: "secret",
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(flash());
+
+// connect Database
+connectDb();
+
+// css image js link public
+app.use(express.static("public"));
+
+// view ejs set
+app.set("view engine", "ejs");
+
+// route load
+app.use("/", web);
+// server create
+app.listen(port, () => {
+  console.log(`server start localhost:${port}`);
+});
